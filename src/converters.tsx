dea11802ea022e43b2f1daeb4723fa1bb0e7a904
hasan <hassanzohdy@gmail.com>
2022-09-27 23:22:31 +0200

    "Initial"

--- src/converters.tsx
+++ src/converters.tsx
@@ -1,21 +1,6 @@
 import React from "react";
 
 /**
- * Convert the placeholders for the given translation
- */
-export function converter(translation: string, placeholders: any) {
-  const expression = `(${Object.keys(placeholders)
-    .map((key) => (key.startsWith(":") ? key : ":" + key))
-    .join("|")})`;
-
-  const regex = new RegExp(expression, "g");
-
-  return translation.replace(regex, (capture: string) => {
-    return placeholders[capture.substring(1)];
-  });
-}
-
-/**
  * JSX converter
  */
 export function jsxConverter(translation: string, placeholders: any) {
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-export * from "./config";
 export * from "./converters";
-export * from "./events";
 export * from "./translator";
-export * from "./types";
